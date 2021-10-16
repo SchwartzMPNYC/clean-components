edc8109fb7cf0538974f7d638866f15aad2f21d6
Mike Schwartz <schwartzmpnyc@gmail.com>
2021-10-16 14:20:22 -0400

    import/export slider

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import ExpandoCard from "./src/components/ExpandoCard/ExpandoCard.component";
+import Slider from "./src/components/Slider/Slider.component";
 
-export { ExpandoCard };
-
+export { ExpandoCard, Slider };
