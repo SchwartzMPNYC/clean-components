6a9707aed7f37ae4823cb0349ef0c48a60210fda
Mike Schwartz <schwartzmpnyc@gmail.com>
2021-10-19 20:04:28 -0400

    TABS!
    First pass at a tabs component. Will almost definitely require some polishing.

---
+++ src/components/Tabs/Tab.component.ts
@@ -0,0 +1,77 @@
+import define from "../../utils/decorators/define/Define";
+import BaseCustomEl from "../Base/Base";
+import styles from "bundle-text:./Tab.styles.scss";
+
+const markup = `<slot></slot>`;
+const observedAttributes = ["selected"];
+
+export type TabChild = HTMLElement | Tab;
+export type TabselectedstatechangeEventDetails = {
+	tab: TabChild;
+	isAttrRemoval?: boolean;
+};
+
+@define("clean-tab", {
+	markup,
+	styles,
+	observedAttributes,
+})
+export default class Tab extends BaseCustomEl {
+	private static booleanReflect = ["selected"];
+
+	public get selected(): boolean {
+		return this.state.selected;
+	}
+	public set selected(newSelected: boolean) {
+		if (newSelected !== this.state.selected) {
+			this.state.selected = newSelected;
+		}
+	}
+
+	connectedCallback() {
+		this.selected = this.hasAttribute("selected");
+		Tab.initTab(this, this.selected);
+
+		super._initState();
+	}
+
+	attributeChangedCallback(name: string, oldVal: string, newVal: string) {
+		if (this.reflecting) return;
+		if (oldVal !== newVal) {
+			if (name === "selected") {
+				Tab.dispatchTabChangeEvent(this, newVal !== "");
+			}
+		}
+	}
+
+	private static dispatchTabChangeEvent(tab: TabChild, isAttrRemoval?: boolean) {
+		const detail: TabselectedstatechangeEventDetails = { tab, isAttrRemoval };
+		tab.dispatchEvent(new CustomEvent("tabselectedstatechange", { detail }));
+	}
+
+	static initTab(tab: TabChild, preselectedState: boolean): void {
+		tab.setAttribute("role", "tab");
+		Tab.setTabState(tab, preselectedState);
+		tab.addEventListener("click", () => Tab.dispatchTabChangeEvent(tab));
+		tab.addEventListener("keydown", event => {
+			if (event.key === " " || event.key === "Enter") {
+				event.preventDefault();
+				Tab.dispatchTabChangeEvent(tab);
+			}
+		});
+
+		// for use in non-library tabs
+		tab.removeAttribute("clean-selected");
+	}
+
+	static tabPreselectedState(tab): boolean {
+		return (tab as Tab).selected ?? tab.hasAttribute("clean-selected");
+	}
+
+	static setTabState(tab: TabChild, selected: boolean) {
+		tab.ariaSelected = selected ? "true" : "false";
+		tab.tabIndex = selected ? 0 : -1;
+
+		if (tab instanceof Tab) tab.selected = selected;
+	}
+}
---
+++ src/components/Tabs/Tabs.component.ts
@@ -0,0 +1,131 @@
+import define from "../../utils/decorators/define/Define";
+import BaseCustomEl from "../Base/Base";
+import styles from "bundle-text:./Tabs.styles.scss";
+import Tab, { TabChild, TabselectedstatechangeEventDetails } from "./Tab.component";
+
+const markup = `<slot></slot>`;
+const observedAttributes = ["selected-index", "manual-activation"];
+
+@define("clean-tabs", {
+	markup,
+	styles,
+	observedAttributes,
+})
+export default class Tabs extends BaseCustomEl {
+	private static reflect = ["selected-index"];
+	private static booleanReflect = ["manual-activation"];
+
+	get manualActivation(): boolean {
+		return this.state.manualActivation ?? false;
+	}
+	set manualActivation(newManualActivation: boolean) {
+		this.state.manualActivation = newManualActivation;
+	}
+
+	get #automaticActivation(): boolean {
+		return !this.manualActivation;
+	}
+
+	#tabs: TabChild[];
+	get selectedIndex(): number {
+		return this.state.selectedIndex;
+	}
+	set selectedIndex(newIndex: number | string) {
+		const newIndexAsNumber = Number(newIndex);
+
+		if (newIndexAsNumber !== this.state.selectedIndex) {
+			this.state.selectedIndex = newIndexAsNumber;
+			this.#focusIndex = newIndexAsNumber;
+			this.#unsetSelectedTabs();
+			if (newIndex !== null) {
+				Tab.setTabState(this.selectedTab, true);
+			}
+			this.#dispatchTabChange(this.selectedTab, this.state.selectedIndex);
+		}
+	}
+
+	#focusIndex = 0;
+
+	get selectedTab(): TabChild {
+		return this.#tabs[this.selectedIndex] ?? null;
+	}
+
+	connectedCallback() {
+		this.setAttribute("role", "tablist");
+		this.#tabs = [...this.children] as TabChild[];
+
+		this.#tabs.forEach((tab: TabChild, tabIndex: number) => {
+			const preselectedState = Tab.tabPreselectedState(tab);
+			// Make sure our tab is initialized for non-clean-tab tabs
+			if (!(tab instanceof Tab)) Tab.initTab(tab, preselectedState);
+			
+			// If tab has a preselected attribute set it to the selected val
+			if (preselectedState) {
+				this.state.selectedIndex = tabIndex;
+				this.#focusIndex = tabIndex;
+			}
+
+			// Tabs will emit this event when selected, so watch for that
+			tab.addEventListener(
+				"tabselectedstatechange",
+				({ detail }: CustomEvent<TabselectedstatechangeEventDetails>) => {
+					this.selectedIndex = detail.isAttrRemoval ? null : tabIndex;
+				}
+			);
+		});
+
+		// The keyboard interactions actually take place on this element rather than the tabs
+		this.addEventListener("keydown", this.#handleKeypress);
+
+		super._initState();
+	}
+
+	attributeChangedCallback(name: string, oldVal: string, newVal: string) {
+		if (this.reflecting) return;
+		if (newVal === oldVal) return;
+
+		switch (name) {
+			case "selected-index":
+				this.selectedIndex = newVal;
+				break;
+
+			case "manual-activation":
+				this.manualActivation = newVal !== null;
+		}
+	}
+
+	#handleKeypress({ key }: KeyboardEvent): void {
+		switch (key) {
+			case "ArrowRight":
+				this.#focusIndex = (this.#focusIndex + 1) % this.#tabs.length;
+				break;
+			case "ArrowLeft":
+				this.#focusIndex = this.#focusIndex !== 0 ? this.#focusIndex - 1 : this.#tabs.length - 1;
+				break;
+			case "Home":
+				this.#focusIndex = 0;
+				break;
+			case "End":
+				this.#focusIndex = this.#tabs.length - 1;
+				break;
+		}
+
+		if (this.#automaticActivation) {
+			this.selectedIndex = this.#focusIndex;
+		}
+
+		this.#getTab(this.#focusIndex).focus();
+	}
+
+	#getTab(index: number) {
+		return this.#tabs[index];
+	}
+
+	#unsetSelectedTabs(): void {
+		this.querySelectorAll('[aria-selected="true"]').forEach((tab: TabChild) => Tab.setTabState(tab, false));
+	}
+
+	#dispatchTabChange(tab: TabChild, tabIndex: number): void {
+		this.dispatchEvent(new CustomEvent("tabselection", { detail: { tab, tabIndex } }));
+	}
+}
