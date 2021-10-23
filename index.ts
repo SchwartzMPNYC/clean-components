e72c163b6aa4e2ae9dcbd255f0d0558c275a7493
Mike Schwartz <schwartzmpnyc@gmail.com>
2021-10-22 22:26:55 -0400

    Export base element and define fucntion so consumers can build custom components

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import ExpandoCard from "./src/components/ExpandoCard/ExpandoCard.component";
 import Slider from "./src/components/Slider/Slider.component";
+import BaseCustomEl from "./src/components/Base/Base";
+import { define } from "./src/utils/decorators/define/Define";
 
-export { ExpandoCard, Slider };
+export { ExpandoCard, Slider, BaseCustomEl, define };
