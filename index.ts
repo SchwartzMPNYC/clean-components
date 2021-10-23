fca8b2a6714b43d1cbaaabc3c6815fcf129c81f1
Mike Schwartz <schwartzmpnyc@gmail.com>
2021-10-23 00:33:18 -0400

    Forgot to export Tabs as part of the package

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import ExpandoCard from "./src/components/ExpandoCard/ExpandoCard.component";
 import Slider from "./src/components/Slider/Slider.component";
+import Tabs from "./src/components/Tabs/Tabs.component";
+import Tab from "./src/components/Tabs/Tab.component";
+
 import BaseCustomEl from "./src/components/Base/Base";
 import { define } from "./src/utils/decorators/define/Define";
 
-export { ExpandoCard, Slider, BaseCustomEl, define };
+export { ExpandoCard, Slider, BaseCustomEl, Tabs, Tab, define };
